85f4ddd03c46c25a0e8d2e3032dd53ce05b66bec
jackPaulino <jacksonpaulino65@gmail.com>
2021-09-27 12:19:47 -0300

    first commit

--- atividade_tres.dart
+++ atividade_tres.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
 
 // 3) Verificar se um número é primo ou não
-class AtividadeTres{
     void main(){
 
  stdout.writeln('Informe o ${(list.length + 1)} numero?');
   var valor = stdin.readLineSync();
     }
 
-}
