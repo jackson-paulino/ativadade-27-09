490b69f9d19afc8b0793038fd2839c612c298960
jackPaulino <jacksonpaulino65@gmail.com>
2021-09-27 12:22:05 -0300

    first commit

--- atividade_tres.dart
+++ atividade_tres.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 // 3) Verificar se um número é primo ou não
     void main(){
 
- stdout.writeln('Informe o ${(list.length + 1)} numero?');
   var valor = stdin.readLineSync();
+ stdout.writeln('Informe o ${(valor)} numero?');
+ print(valor);
     }
 
